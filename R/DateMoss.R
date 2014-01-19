abf32f700fa84cab139345018f241393dcfc413f
Simon Wotherspoon <Simon.Wotherspoon@utas.edu.au>
2014-01-20 09:44:17 +1100

    Builds but does nothing

---
+++ R/DateMoss.R
@@ -0,0 +1,10 @@
+#' Chronologies and growth rates of moss
+#'
+#' Provides facilities for e Estimating chronologies and growth rates
+#' of moss (or similar) samples based on radiocarbon dating.
+#'
+#' @name MossDate-package
+#' @docType package
+#' @author B. Raymond, S. Wotherspoon
+NULL
+
